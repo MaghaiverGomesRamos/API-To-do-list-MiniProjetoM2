0659ef9a85edc6276db345d7529a8bdbdf18bc9d
Breakosts <magaiverg2@gmail.com>
2026-01-28 20:49:42 -0300

    corrigindo erros

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,10 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
-import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
-const config = require('../../config/config.json');
-
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
-
-const sequelize = dbConfig.use_env_variable
-  ? new Sequelize(process.env[dbConfig.use_env_variable], dbConfig)
-  : new Sequelize(dbConfig);
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+  logging: false
+});
 
 export default sequelize;
