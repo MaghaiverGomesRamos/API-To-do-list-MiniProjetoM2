ed262a032174fec42aee2cacbfa7c049bb046903
Breakosts <magaiverg2@gmail.com>
2026-01-28 22:48:08 -0300

    adicionando cors

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';               
 import router from './routes/tarefaRoutes.js';
 
 const app = express();
+
+app.use(cors());                      
 app.use(express.json());
 
 // Define o grupo de rotas relacionadas a tarefas
 app.use('/tarefas', router);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 // Inicia o servidor e exibe no console a porta usada
 app.listen(PORT, () => {
